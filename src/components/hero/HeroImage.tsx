178f6c576962fb25e0bf31d0c7e373e0cad1774e
Soarintermediary <barney.sykes@soarbusinessgroup.com>
2025-06-09 14:25:31 +0400

    Update: Hero image component changes

--- src/components/hero/HeroImage.tsx
+++ src/components/hero/HeroImage.tsx
@@ -5,7 +5,7 @@ const HeroImage: FC = () => {
     <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
       <img
         className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-        src="https://images.pexels.com/photos/3885593/pexels-photo-3885593.jpeg"
+        src="https://images.pexels.com/photos/2115367/pexels-photo-2115367.jpeg"
         alt="Happy business traveler walking through airport"
       />
     </div>
